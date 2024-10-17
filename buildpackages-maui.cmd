2b449506e165773998d151bb04473ff593fa464a
Jonas Follesø <jonas@follesoe.no>
2024-10-17 12:57:36 +0200

    Add workflow to build and publish MAUI package

--- buildpackages-maui.cmd
+++ buildpackages-maui.cmd
@@ -1,3 +1,6 @@
 @echo off
 dotnet build --configuration=Release Sources\Microcharts\Microcharts.csproj
+dotnet build --configuration=Release Sources\Microcharts.Maui\Microcharts.Maui.csproj
 dotnet build --configuration=Release Sources\Microcharts.Samples.Maui\Microcharts.Samples.Maui.csproj
+
+dotnet pack --configuration=Release Sources\Microcharts.Maui\Microcharts.Maui.csproj
