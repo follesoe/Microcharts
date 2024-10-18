6393b1bbb5cdefdb9b1f6c96ab8b0dbc0c7526a0
Jonas Follesø <jonas@follesoe.no>
2024-10-18 11:10:03 +0200

    Build and publish Microcharts.Core package

--- buildpackages-maui.cmd
+++ buildpackages-maui.cmd
@@ -3,4 +3,5 @@ dotnet build --configuration=Release Sources\Microcharts\Microcharts.csproj
 dotnet build --configuration=Release Sources\Microcharts.Maui\Microcharts.Maui.csproj
 dotnet build --configuration=Release Sources\Microcharts.Samples.Maui\Microcharts.Samples.Maui.csproj
 
+dotnet pack --configuration=Release Sources\Microcharts\Microcharts.csproj
 dotnet pack --configuration=Release Sources\Microcharts.Maui\Microcharts.Maui.csproj
