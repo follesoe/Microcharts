beec3ba04c38d128feeb216593f24703d4b41518
Jonas Follesø <jonas@follesoe.no>
2025-02-10 09:38:41 +0100

    Delete Uno, Avalonia and Eto support
    Reduce test area and narrow down support to make it easier to maintain the library. These platforms can still use the core package, and do their own binding towards SkiaSharp on the respective platforms.

--- buildpackages.cmd
+++ buildpackages.cmd
@@ -5,10 +5,6 @@ msbuild /t:build,pack /p:Configuration=Release Sources\Microcharts.Droid\Microch
 msbuild /t:build,pack /p:Configuration=Release Sources\Microcharts.iOS\Microcharts.iOS.csproj
 rem msbuild /t:build,pack /p:Configuration=Release Sources\Microcharts.macOS\Microcharts.macOS.csproj
 msbuild /t:build,pack /p:Configuration=Release Sources\Microcharts.Uwp\Microcharts.Uwp.csproj
-msbuild /t:build,pack /p:Configuration=Release Sources\Microcharts.Avalonia\Microcharts.Avalonia.csproj
-msbuild /t:build,pack /p:Configuration=Release Sources\Microcharts.Eto\Microcharts.Eto.csproj
 msbuild /t:build,pack /p:Configuration=Release Sources\Microcharts.Maui\Microcharts.Maui.csproj
-msbuild /t:build,pack /p:Configuration=Release Sources\Microcharts.Uno\Microcharts.Uno.csproj
-msbuild /t:build,pack /p:Configuration=Release Sources\Microcharts.Uno.WinUI\Microcharts.Uno.WinUI.csproj
 msbuild /t:build,pack /p:Configuration=Release Sources\Microcharts.WinUI\Microcharts.WinUI.csproj
 dotnet pack Sources\Microcharts.Metapackage\Microcharts.Metapackage.csproj
