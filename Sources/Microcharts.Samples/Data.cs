3c77002cdc24aca0d5218b41ef6c451023208d8e
Jonas Follesø <jonas@follesoe.no>
2025-02-10 13:14:20 +0100

    Ignore obosolete warning for this class
    It contains legacy code examples that the library supports but is marked as obsolete.

--- Sources/Microcharts.Samples/Data.cs
+++ Sources/Microcharts.Samples/Data.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using SkiaSharp;
+#pragma warning disable CS0618 // Type or member is obsolete
 
 namespace Microcharts.Samples
 {
